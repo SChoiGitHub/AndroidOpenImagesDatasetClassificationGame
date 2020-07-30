c1617ddb6160c3614ce2e3ed0174eb5cc6f3f5f4
Sherman Choi <22206740+SChoiGitHub@users.noreply.github.com>
2020-07-30 11:59:49 -0500

    Loading images into data class now

--- app/src/main/java/com/example/classifythis/QuizViewModel.kt
+++ app/src/main/java/com/example/classifythis/QuizViewModel.kt
@@ -1,15 +1,20 @@
 package com.example.classifythis
 
 import android.app.Application
-import android.content.Context
-import android.content.res.Resources
+import android.graphics.BitmapFactory
 import android.util.Log.d
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import kotlin.random.Random
+
 
 class QuizViewModel(val app : Application) : AndroidViewModel(app) {
+    data class Image(val url: String, val xMin: Float, val xMax: Float, val yMin: Float, val yMax: Float, val classification: String)
+
+    var currentDisplayedImages = ArrayList<Image>(4)
+    var allImages = ArrayList<Image>()
+
     init{
         try {
             var csvFilepath = getApplication<Application>().resources.openRawResource(R.raw.sample)
@@ -19,13 +24,46 @@ class QuizViewModel(val app : Application) : AndroidViewModel(app) {
             var row : String? = csvReader.readLine()
             while(row != null){
                 val values = row.split(",")
-
-                row?.let { d("What", values.toString()) }
+                val currentImage =  Image(
+                    values[0],
+                    values[1].toFloat(),
+                    values[2].toFloat(),
+                    values[3].toFloat(),
+                    values[4].toFloat(),
+                    values[5])
+                allImages.add(currentImage)
                 row = csvReader.readLine()
             }
         } catch (e : Exception) {
-            e.message?.let { d("What", it) }
+            e.message?.let { d("ERROR", it) }
         }
 
+    }
+
+    fun downloadImages(img : Image){
+        val stream = java.net.URL(img.url).openStream()
+        val picture = BitmapFactory.decodeStream(stream)
+    }
+
+    fun selectImages(){
+        //Get four unique indices
+        var selectedIndices = setOf(
+            (0 until allImages.size).random(),
+            (0 until allImages.size).random(),
+            (0 until allImages.size).random(),
+            (0 until allImages.size).random()
+        )
+        while(selectedIndices.size != 4){
+            selectedIndices = setOf(
+                (0 until allImages.size).random(),
+                (0 until allImages.size).random(),
+                (0 until allImages.size).random(),
+                (0 until allImages.size).random()
+            )
+        }
+
+        for(i in selectedIndices){
+
+        }
     }
 }
