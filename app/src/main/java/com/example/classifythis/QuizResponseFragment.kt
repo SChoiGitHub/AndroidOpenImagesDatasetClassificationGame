03faa5f3d79b8e9092352a0edfdc1731f1173d7d
Sherman Choi <22206740+SChoiGitHub@users.noreply.github.com>
2020-07-31 17:07:20 -0500

    start loading next question immediately

--- app/src/main/java/com/example/classifythis/QuizResponseFragment.kt
+++ app/src/main/java/com/example/classifythis/QuizResponseFragment.kt
@@ -8,14 +8,11 @@ import androidx.fragment.app.DialogFragment
 
 class QuizResponseFragment(val text : String, val callback : () -> Unit) : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        callback()
         return activity?.let {
             val builder = AlertDialog.Builder(it)
             builder.setMessage(text)
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
-    }
-
-    override fun onCancel(dialog: DialogInterface) {
-        callback()
     }
 }
